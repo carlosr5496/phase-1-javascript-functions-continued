627bc2f5f85a35c68789dbea79ae08cd9265bae6
Ryan Carlos <ryancarlos925@gmail.com>
2022-04-13 23:18:27 -0700

    commit tree acc728fed6fdfef5257c60f78a3689d16f0ceae2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun (task="roller-skate") {
+    return (`This Saturday, I want to ${task}!`)
+} 
+
+
+const mondayWork = function(task="go to the office") {
+    let resp = `This Monday, I will ${task}.`
+    return resp 
+};
+
+function wrapAdjective(spec="*") {
+    const innerFunction = function (x="special") {
+        return `You are ${spec}${x}${spec}!`;
+    };
+    return innerFunction;
+}
+    // const encouragingPromptFunction = wrapAdjective("!!!")
+    // wrapAdjective("%")("a dedicated programmer")
+
+
+    // function wrapAdjective(spec="*") {
+    //     const innerFunction = function (x="special") {
+    //         return `You are ${spec}a hard worker${spec}!`;
+    //     };
+    //     return innerFunction;
